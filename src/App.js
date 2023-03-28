c54d83e76464d8c837859a7e849b3ed75e23b5f7
caelidh <ceilidhliddell@gmail.com>
2023-03-27 18:27:38 -0700

    removed extra closing tag on link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Weather />
-      <p>This page was coded by Caelidh Liddell and the code can be seen on <a link="https://github.com/caelidh/react-weather-app">Github</a></a></p>
+      <p>This page was coded by Caelidh Liddell and the code can be seen on <a link="https://github.com/caelidh/react-weather-app">Github</a></p>
     </div>
   );
 }
