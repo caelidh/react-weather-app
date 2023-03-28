75e8dc07a4e97df9f2df25dd4f4fabc7a269a054
caelidh <ceilidhliddell@gmail.com>
2023-03-27 18:34:39 -0700

    moved paragraph section

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
+      <p>This page was coded by Caelidh Liddell and the code can be seen on <a href="https://github.com/caelidh/react-weather-app">Github</a></p>
+
       <Weather />
       
     </div>
   );
 }
 
-<p>This page was coded by Caelidh Liddell and the code can be seen on <a href="https://github.com/caelidh/react-weather-app">Github</a></p>
