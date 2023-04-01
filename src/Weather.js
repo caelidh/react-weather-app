b992e31a6e72009efe78b3cf1f47c9d1c8b7dbe1
caelidh <ceilidhliddell@gmail.com>
2023-03-31 18:18:16 -0700

    added formatted date file

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
+import FormattedDate from "./FormattedDate";
 
 
 export default function Weather() {
@@ -21,6 +23,7 @@ export default function Weather() {
     console.log(response);
 
     setWeather({
+      date: new Date(response.data.dt*1000),
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
@@ -31,6 +34,7 @@ export default function Weather() {
 
   return (
     <div className="Weather">
+      <div className="container">
       <form onSubmit={handleSubmit}>
         <input
           type="search"
@@ -38,9 +42,11 @@ export default function Weather() {
           onChange={updateCity}
         />
         <input type="submit" value="Search" />
+      
       </form>
       {weather ? (
         <ul>
+          <li><FormattedDate date={weather.date} /></li>
           <li>Temperature: {Math.round(weather.temperature)}°F</li>
           <li>Description: {weather.description}</li>
           <li>Humidity: {weather.humidity}%</li>
@@ -49,6 +55,7 @@ export default function Weather() {
           <img alt="icon" src={weather.icon} />
         </ul>
       ) : null}
+    </div>
     </div>
   );
 }
