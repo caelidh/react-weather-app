a294199e32a946f18a352079fc563f4d706a0af5
caelidh <ceilidhliddell@gmail.com>
2023-03-31 17:40:32 -0700

    added div

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather Search Engine</h1>
       <p>This page was coded by Caelidh Liddell and the code can be seen on <a href="https://github.com/caelidh/react-weather-app">Github</a></p>
 
       <Weather />
-      
+      </div>
     </div>
   );
 }
